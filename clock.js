d0c6914d7b80efa9adfe4167184b7385105c7f56
Gabe <gabe1118@yahoo.com>
2016-05-29 00:37:16 -0400

    clock test?

---
+++ clock.js
@@ -0,0 +1,13 @@
+function updateClock(){
+  var t = getTimeRemaining(endtime);
+  clock.innerHTML = 'days: ' + t.days + '<br>' +
+                    'hours: '+ t.hours + '<br>' +
+                    'minutes: ' + t.minutes + '<br>' +
+                    'seconds: ' + t.seconds;
+  if(t.total<=0){
+    clearInterval(timeinterval);
+  }
+}
+
+updateClock(); // run function once at first to avoid delay
+var timeinterval = setInterval(updateClock,1000);
